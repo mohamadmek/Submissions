731cbecbb2804913c7521a1ad01c34886860cec5
MohamadMeksasi <mouhamadmeksasi@gmail.com>
2020-01-13 14:48:04 +0200

    Weather API: HTML/CSS

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import storm from "../src/img/weather-icons/storm.svg";
+import cloudy from "../src/img/weather-icons/partlycloudy.svg";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -10,9 +11,80 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <header className="app__header"></header>
-        <main className="app__main"></main>
-        name
+        <nav>
+          <label for="city" className="label-city">
+            <input type="text" name="city" className="city"></input>
+          </label>
+          <button className="btn-search">FIND WEATHER</button>
+        </nav>
+        <div className="container">
+          <div className="today-container">
+            <div className="today-icon">
+              <img className="img" src={cloudy} alt="storm icon" />
+            </div>
+            <div className="today-txt">overcast clouds</div>
+            <div className="mt-25">
+              <span className="bold">Temperature</span>10 to 11C
+            </div>
+            <div className="mt-25">
+              <span className="bold">Humidity</span> 78%
+              <span className="bold2">Pressure</span> 1008.48
+            </div>
+          </div>
+          <div className="line"></div>
+          <div className="hourly-title">HOURLY</div>
+          <div className="row">
+            <div className="hours-weather b-r b-l b-rl">
+              <div className="hour">03:00</div>
+              <div className="icon">
+                <img src={storm} alt="stormy"></img>
+              </div>
+              <div>8C</div>
+            </div>
+            <div className="hours-weather b-r b-rl">
+              <div className="hour">03:00</div>
+              <div className="icon">
+                <img src={storm} alt="stormy"></img>
+              </div>
+              <div>8C</div>
+            </div>
+            <div className="hours-weather b-r b-rl">
+              <div className="hour">03:00</div>
+              <div className="icon">
+                <img src={storm} alt="stormy"></img>
+              </div>
+              <div>8C</div>
+            </div>
+            <div className="hours-weather b-r b-l b-rl">
+              <div className="hour">03:00</div>
+              <div className="icon">
+                <img src={storm} alt="stormy"></img>
+              </div>
+              <div>8C</div>
+            </div>
+            <div className="hours-weather b-r b-rl">
+              <div className="hour">03:00</div>
+              <div className="icon">
+                <img src={storm} alt="stormy"></img>
+              </div>
+              <div>8C</div>
+            </div>
+            <div className="hours-weather b-r b-rl">
+              <div className="hour">03:00</div>
+              <div className="icon">
+                <img src={storm} alt="stormy"></img>
+              </div>
+              <div>8C</div>
+            </div>
+            <div className="hours-weather  last b-rl">
+              <div className="hour">03:00</div>
+              <div className="icon">
+                <img src={storm} alt="stormy"></img>
+              </div>
+              <div>8C</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
