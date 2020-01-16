e9ce9218815a66ce9b18f0402ca22be5f1afa5d2
MohamadMeksasi <mouhamadmeksasi@gmail.com>
2020-01-16 21:46:44 +0200

    Basic Queries

---
+++ Exercises/SQLite Basic/answers.sh
@@ -0,0 +1,20 @@
+1- SELECT name FROM students
+
+2- SELECT * FROM students WHERE Age > 30
+
+3- SELECT name FROM students
+   WHERE Age = 30 AND Gender = "F"
+
+4- SELECT Points FROM students
+   WHERE name = "Alex"
+
+5- INSERT INTO students (Name,Age,Gender,Points)
+   VALUES ("Mohamad",22,"M",200)
+
+6- UPDATE students
+   SET Points = 350
+   WHERE Name = "Basma"
+
+7- UPDATE students
+   SET Points = 150
+   WHERE Name = "Alex"
