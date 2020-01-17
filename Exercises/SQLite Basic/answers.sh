9f52cd8ace4d41ecbe8285fc5cc85cfc38117a16
MohamadMeksasi <mouhamadmeksasi@gmail.com>
2020-01-17 15:31:58 +0200

    Count & Filter

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -55,3 +55,24 @@
     FROM companies
     INNER JOIN employees ON companies.Name=employees.Company
     WHERE employees.Role = "Graphic Designer"
+
+14- Solution 1:
+    SELECT Name from (select Name ,max(Points) from students)
+    Solution 2:
+    SELECT Name
+    FROM students
+    ORDER by Points DESC
+    LIMIT 1
+
+15- SELECT AVG(Points) From students
+
+16- SELECT count(Name)
+    FROM students 
+    WHERE Points = 500
+
+17- SELECT Name
+    FROM students
+    WHERE Name like'%s%' 
+
+18- SELECT Name FROM students
+    ORDER by Points DESC
