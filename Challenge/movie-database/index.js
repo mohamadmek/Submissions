221b358fb6b6974ca69220582d3542fb45f19fe4
MohamadMeksasi <mouhamadmeksasi@gmail.com>
2020-01-22 14:05:56 +0200

    step5

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -2,6 +2,29 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const movies = [
+  { title: "Jaws", year: 1975, rating: 8 },
+  { title: "Avatar", year: 2009, rating: 7.8 },
+  { title: "Brazil", year: 1985, rating: 8 },
+  { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 }
+];
+
+app.get("/movies/add", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+
+app.get("/movies/get", (req, res) => {
+  res.send({ status: 200, message: "ok", data: movies });
+});
+
+app.get("/movies/edit", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+
+app.get("/movies/delete", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+
 app.get("/test", (req, res) => {
   res.send({ status: 200, message: "ok" });
 });
@@ -29,4 +52,5 @@ app.get("/time", (req, res) => {
   let min = date.getMinutes();
   res.send({ status: 200, message: `${hours}:${min}` });
 });
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
