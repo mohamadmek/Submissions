dc6bf244a7edc64ab8020c92b6a18c54b489e927
MohamadMeksasi <mouhamadmeksasi@gmail.com>
2020-01-22 14:00:45 +0200

    Step-4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -6,6 +6,23 @@ app.get("/test", (req, res) => {
   res.send({ status: 200, message: "ok" });
 });
 
+app.get("/test/:id", (req, res) => {
+  let id = req.params.id;
+  res.send({ status: 200, message: `hello ${id}` });
+});
+
+app.get("/search?", (req, res) => {
+  if (req.query.s) {
+    const search = req.query.s;
+    res.send({ status: 200, message: "ok", data: search });
+  } else {
+    res.send({
+      status: 500,
+      error: true,
+      message: "you have to provide a search"
+    });
+  }
+});
 app.get("/time", (req, res) => {
   let date = new Date();
   let hours = date.getHours();
