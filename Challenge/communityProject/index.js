f7db801c9ef82093a7d9abb6b5db74e69ca0afbd
MohamadMeksasi <mouhamadmeksasi@gmail.com>
2020-01-27 14:05:53 +0200

    a better version for the community

--- Challenge/communityProject/index.js
+++ Challenge/communityProject/index.js
@@ -13,20 +13,38 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
-
 const sliderColumns = document.querySelectorAll(".column");
+const c2 = document.querySelector(".c2");
+const c1 = document.querySelector(".c1");
+const c3 = document.querySelector(".c3");
+// const logo = document.querySelector(".logo")
 function checkSlide(e) {
-  sliderColumns.forEach(sliderColumn => {
-    const slideInAt =
-      window.scrollY + window.innerHeight - sliderColumn.height / 2;
-    const imageBottom = sliderColumn.offsetTop + sliderColumn.height;
-    const isHalfShown = slideInAt > sliderColumn.offsetTop;
-    const isNotScrollPast = window.scrollY < imageBottom;
-    if (isHalfShown) {
-      sliderColumn.classList.add("column-active");
-    } else {
-      console.log("asflnjsdg");
-    }
+  // sliderColumns.forEach(sliderColumn => {
+  //   const slideInAt = window.scrollY + window.innerHeight ;
+  //   console.log(window.scrollY)
+  //   const imageBottom = sliderColumn.offsetTop + sliderColumn.height;
+  //   const isHalfShown = slideInAt > sliderColumn.offsetTop;
+  if (window.scrollY > 332) {
+    c2.classList.add("c2-active");
+    c1.classList.add("c1-active");
+    c3.classList.add("c3-active");
+    // logo.classList.add("logo-active")
+  } else {
+    console.log("asflnjsdg");
+  }
+}
+
+function loadNav(e) {
+  const h1 = document.querySelector(".h1");
+  const h2 = document.querySelector(".h2");
+  const h3 = document.querySelector(".h3");
+  const links = document.querySelectorAll(".link-nav");
+  h1.classList.add("h-active");
+  h2.classList.add("h-active");
+  h3.classList.add("h-active");
+  links.forEach(link => {
+    link.classList.add("h-active");
   });
 }
 window.addEventListener("scroll", debounce(checkSlide));
+window.addEventListener("load", loadNav);
